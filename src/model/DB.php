64e595e2ea87651e03bd28a980f2b64a48785d81
Moussa SARR <einsarr@gmail.com>
2019-12-19 15:42:29 +0000

    Ajout de la classe DB

--- src/model/DB.php
+++ src/model/DB.php
@@ -1,15 +1,5 @@
 <?php
-function getConnection(){
-    $host = "127.0.0.1";
-    $user = "root";
-    $password = "";
-    $dbname = "geolocalisation";
-    $dsn="mysql:host=$host;dbname=$dbname";
-
-    try{
-        $pdo = new PDO($dsn,$user,$password);
-    }catch(Exception $ex){
-        die('error'.$ex->getMessage());
-    }
-    return $pdo;
+class DB
+{
+    
 }
--- src/model/localiteDB.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "DB.php";
-
-function add($nom,$latitude,$longitude){
-    $pdo = getConnection();
-    $req = "INSERT INTO localite VALUES(NULL,'$nom',$latitude,$longitude)";
-    $pdo->exec($req);
-}
-
